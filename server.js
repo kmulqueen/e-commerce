bfeb92a7fe9d187634ab79f5a7e2e1ea6e6fc1a9
Rafael Rodriguez <rafaelrodriguez@Rafaels-MacBook-Pro.local>
2019-05-04 14:21:37 -0600

    Got routes working 5-4-19

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const mongoose = require('mongoose');
-
+const bodyParser = require("body-parser");
 const users = require('./routes/api/users');
 const store = require('./routes/api/store');
+const passport = require("passport");
 
 const app = express();
 
 // DB config 
 const db = require('./config/keys').mongoURI;
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 
 // Connect to MongoDb
 mongoose
@@ -18,6 +24,12 @@ mongoose
 app.get("/", (req, res) => {
   res.send("Home Page");
 });
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require("./config/passport")(passport);
 
 //Use the routes
 app.use('/api/users', users);
